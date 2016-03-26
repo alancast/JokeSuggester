fff65f152b1cd4ce7b5327d15b90a398d07344fb
Alex <alancast@umich.edu>
2016-03-26 17:29:29 -0400

    Major update (fortunately we are sitting next to each other for most of it).
    -Created a “createUserStatistics” that creates creation date and
    follower count of all twitter profiles we are pulling from (but only
    ones that haven’t already gotten statistics from before). All these
    stats are stored in the “user statistics.csv”
    -Updated scrape twitter to be more robust (sleep time so that hopefully
    we don’t go over API rate limits, better error catching)
    -Added many datasets for twitter profiles
    -Created “countTotalTweets.py” which will count how many total tweets
    we have in the dataset so far and how many from each user

---
+++ createUserStatistics.py
@@ -0,0 +1,67 @@
+import tweepy
+import csv
+import sys
+import os
+from sets import Set
+
+# These keys and secrets are for the "Joke Suggester" app 
+# on Shlancaster39's account
+consumer_key = 'L9jRZtrpgbJNu6Z9fPMfxPiZq'
+consumer_secret = '5AkgpcNeRAc4IIB6kolZIjJ9DWH6g31ndx5LeLTZZtQqfbllGP'
+access_token = '599251230-V3Tl1N3qnlgpd0PViBFB0aclXiVuEaaKMd3KQBUA'
+access_token_secret = 'MY8DqQ3sbECkXU0iUr6MfRdasWIaDW8MKc5DuH2G8VBfG'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# PURPOSE: Creates a set of all usernames we have already analyzed
+# INPUTS: string of file name
+# RETURNS: Set of usernames already gotten
+def GetUsersFromFile(file):
+    analyzed_set = Set()
+    with open(file, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            analyzed_set.add(row[0])
+    return analyzed_set
+
+# PURPOSE: Analyzes everything we need about the passed in use. Utilizes tweepy API
+# INPUTS: username of uswer we want to analyze
+# RETURNS: list of all the stats needed to write to csv
+def AnalyzeUser(username):
+    stats = [username]
+    user = api.get_user(username)
+    stats.append(user.followers_count)
+    stats.append(user.created_at)
+    return stats
+
+# PURPOSE: Writes one row to CSV file with all the user stats gathered
+# INPUTS: list of user stats, outfile name
+# RETURNS: nothing
+def WriteRow(user_stats, outfile):
+    writer = csv.writer(open(outfile,'a'))
+    writer.writerow(user_stats)
+
+# PURPOSE: update csv for twitter user info
+#           CSV file format: username, follower_count, creation date
+# COMMAND LINE ARGS: [directory name (with the /),
+#                       filename and location we are writing to]
+def main(argv):
+    directory_name = argv[0]
+    outfile_name = argv[1]
+    analyzed_set = GetUsersFromFile(outfile_name)
+    for file in os.listdir(directory_name):
+        if not file.startswith('.'):
+            username = file[:-11]
+            if username not in analyzed_set:
+                print "Adding new user:", username
+                user_stats = AnalyzeUser(username)
+                WriteRow(user_stats, outfile_name)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
+
--- scrapetwitter.py
+++ scrapetwitter.py
@@ -3,6 +3,7 @@ import csv
 import sys
 import os
 from sets import Set
+import time
 
 # These keys and secrets are for the "Joke Suggester" app 
 # on Shlancaster39's account
@@ -23,32 +24,40 @@ api = tweepy.API(auth)
 def get_all_tweets_for_user(screen_name):
     # Twitter only allows access to a users most recent 3240 tweets with this method
     # Initialize a list to hold all the tweepy Tweets
-    all_tweets = []  
-    # Make initial request for most recent tweets (200 is the maximum allowed count)(?)
-    new_tweets = api.user_timeline(screen_name = screen_name,count=200, 
-                                    exclude_replies=True, include_rts=False)
+    all_tweets = []
+    try:
+        # Make initial request for most recent tweets (200 is the maximum allowed count)(?)
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200, 
+                                        exclude_replies=True, include_rts=False)
+    except tweepy.TweepError, e:
+        print "Caught some tweepy exception:", e
+        if type(e) == tweepy.error.RateLimitError:
+            sys.exit()
+        return
     # Make sure no tweets are quote tweets
     no_quoted_new_tweets = []
     for tweet in new_tweets:
         # Make sure tweet isn't a quote tweet
         if hasattr(tweet, 'quoted_status'):
-            # print "Found quote tweet, don't include it"
             continue
         no_quoted_new_tweets.append(tweet)
     all_tweets.extend(no_quoted_new_tweets)
     oldest = all_tweets[-1].id - 1
     # Keep grabbing tweets until there are no tweets left to grab
-    while len(new_tweets) > 0:
+    while len(no_quoted_new_tweets) > 0:
         print "getting tweets before %s" % (oldest)
-        # All subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest,
-                                        exclude_replies=True, include_rts=False)
+        try:
+            # All subsiquent requests use the max_id param to prevent duplicates
+            new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest,
+                                            exclude_replies=True, include_rts=False)
+        except tweepy.TweepError, e:
+            print "Caught some tweepy exception:", e
+            sys.exit()
         # Make sure no tweets are quote tweets
         no_quoted_new_tweets = []
         for tweet in new_tweets:
             # Make sure tweet isn't a quote tweet
             if hasattr(tweet, 'quoted_status'):
-                # print "Found quote tweet, don't include it"
                 continue
             no_quoted_new_tweets.append(tweet)
         all_tweets.extend(no_quoted_new_tweets)
@@ -60,6 +69,8 @@ def get_all_tweets_for_user(screen_name):
 # INPUTS: list of tweepy tweets, twitter handle of user who tweets belong to
 # RETURNS: nothing
 def output_tweets(tweets, screen_name):
+    if tweets == None:
+        return
     # Transform the tweepy tweets into a 2D array that will populate the csv 
     outtweets = [[tweet.text.encode("utf-8"), tweet.created_at, tweet.favorite_count, tweet.retweet_count, tweet.lang] for tweet in tweets]
     # Write the csv  
@@ -77,12 +88,14 @@ def GetUsersFromDirectory(directory):
         gotten_set.add(file[:-11])
     return gotten_set
 
+# Counts the number of data rows (tweets) in an entire directory
+# COMMAND LINE ARGS: [filename and location of list of twitter accounts,
+#                       directory name (with the /) of dataset of tweets]
 def main(argv):
     infile_name = argv[0]
     directory_name = argv[1]
     # Populate set of usernames we have already finished
     done_set = GetUsersFromDirectory(directory_name)
-    print done_set
     # Read in file with list of users we are going to search
     with open(infile_name) as f:
         for line in f:
@@ -94,6 +107,9 @@ def main(argv):
             print name
             tweets = get_all_tweets_for_user(name)
             output_tweets(tweets, name)
+            print "Sleeping for .75 minutes starting from:", time.strftime("%H:%M:%S")
+            time.sleep(45)
+            print "Done sleeping at:", time.strftime("%H:%M:%S")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
